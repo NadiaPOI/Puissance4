09f8d0bc77bbd19c40d932758046262a3f09b941
NadiaPoi <nadia.poi.fou@gmail.com>
2019-03-19 17:01:27 +0100

    update addPawn.test.js

--- addPawn.test.js
+++ addPawn.test.js
@@ -2,29 +2,32 @@ const addPawn = require("./addPawn");
 const generateGameboard = require("./generateGameboard");
 
 describe("addPawn", function(){
-    var gameboard = generateGameboard(6, 7);
     it("should add a pawn to gameboard", function(){
+        var gameboard = generateGameboard(6, 7);
         expect(addPawn(gameboard, 6)).toEqual([["","","","","","",""],
-                                                     ["","","","","","",""],
-                                                     ["","","","","","",""],
-                                                     ["","","","","","",""],
-                                                     ["","","","","","",""],
-                                                     ["","","","","","","Y"]]);
+                                               ["","","","","","",""],
+                                               ["","","","","","",""],
+                                               ["","","","","","",""],
+                                               ["","","","","","",""],
+                                               ["","","","","","","Y"]]);
     })
     it("should add a pawn to gameboard", function(){
+        var gameboard = generateGameboard(6, 7);
         expect(addPawn(gameboard, 0)).toEqual([["","","","","","",""],
-                                                     ["","","","","","",""],
-                                                     ["","","","","","",""],
-                                                     ["","","","","","",""],
-                                                     ["","","","","","",""],
-                                                     ["Y","","","","","","Y"]]);
+                                               ["","","","","","",""],
+                                               ["","","","","","",""],
+                                               ["","","","","","",""],
+                                               ["","","","","","",""],
+                                               ["Y","","","","","",""]]);
     })
     it("should add a pawn to gameboard", function(){
+        var gameboard = generateGameboard(6, 7);
+        gameboard[5][0] = "Y";
         expect(addPawn(gameboard, 0)).toEqual([["","","","","","",""],
-                                                     ["","","","","","",""],
-                                                     ["","","","","","",""],
-                                                     ["","","","","","",""],
-                                                     ["Y","","","","","",""],
-                                                     ["Y","","","","","","Y"]]);
+                                               ["","","","","","",""],
+                                               ["","","","","","",""],
+                                               ["","","","","","",""],
+                                               ["Y","","","","","",""],
+                                               ["Y","","","","","",""]]);
     })    
 })
