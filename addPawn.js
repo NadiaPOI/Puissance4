7c083e87e4e862329a7564742a235925f65dcf1c
NadiaPoi <nadia.poi.fou@gmail.com>
2019-03-22 11:25:16 +0100

    update all tests of addPawn.test.js and update function addPawn

--- addPawn.js
+++ addPawn.js
@@ -1,15 +1,25 @@
+const gameboard = require("./generateGameboard");
+
 function addPawn(gameboard,column){
 
-    const lastRow = gameboard.length -1;
+    let lastRow = gameboard.length - 1;
 
-        if(gameboard[lastRow][column] === ""){
-            gameboard[lastRow][column] = "Y";
-        } else {
-            gameboard[lastRow - 1][column] = "Y";
-        }
+    if(!gameboard[0].includes("")){
+        throw new Error("The gameboard is full");
+    }
 
+    if(gameboard[0][column] === "Y"){
+        throw new Error("The column is full");
+    }
+
+    while(gameboard[lastRow][column] === "Y" && lastRow > 0){
+            lastRow--;
+    }
+    gameboard[lastRow][column] = "Y";
+    
     console.log(gameboard);
     return gameboard;
+    
 }
 
 module.exports = addPawn;
--- addPawn.test.js
+++ addPawn.test.js
@@ -1,47 +1,130 @@
 const addPawn = require("./addPawn");
-const generateGameboard = require("./generateGameboard");
 
 describe("addPawn", function(){
-    it("should add a pawn to gameboard", function(){
-        var gameboard = generateGameboard(6, 7);
-        expect(addPawn(gameboard, 6)).toEqual([["","","","","","",""],
-                                               ["","","","","","",""],
-                                               ["","","","","","",""],
-                                               ["","","","","","",""],
-                                               ["","","","","","",""],
-                                               ["","","","","","","Y"]]);
-    })
-    it("should add a pawn to gameboard", function(){
-        var gameboard = generateGameboard(6, 7);
-        expect(addPawn(gameboard, 0)).toEqual([["","","","","","",""],
-                                               ["","","","","","",""],
-                                               ["","","","","","",""],
-                                               ["","","","","","",""],
-                                               ["","","","","","",""],
-                                               ["Y","","","","","",""]]);
-    })
-    it("should add a pawn to gameboard", function(){
-        var gameboard = generateGameboard(6, 7);
-        gameboard[5][0] = "Y";
-        expect(addPawn(gameboard, 0)).toEqual([["","","","","","",""],
-                                               ["","","","","","",""],
-                                               ["","","","","","",""],
-                                               ["","","","","","",""],
-                                               ["Y","","","","","",""],
-                                               ["Y","","","","","",""]]);
-    })
-    it("should add a pawn to gameboard", function(){
-        var gameboard = new Array(6);
+    it("should add a pawn to column 6 when gameboard is empty", function(){
+        var gameboard = [
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""]
+        ];
 
-        for(var row = 0; row < gameboard.length; row++){
-            gameboard[row] = ["Y", "Y", "Y", "Y", "Y", "Y", "Y"];
-        }
-           
-        expect(addPawn(gameboard, 0)).toEqual([["Y", "Y", "Y", "Y", "Y", "Y", "Y"],
-                                               ["Y", "Y", "Y", "Y", "Y", "Y", "Y"],
-                                               ["Y", "Y", "Y", "Y", "Y", "Y", "Y"],
-                                               ["Y", "Y", "Y", "Y", "Y", "Y", "Y"],
-                                               ["Y", "Y", "Y", "Y", "Y", "Y", "Y"],
-                                               ["Y", "Y", "Y", "Y", "Y", "Y", "Y"]]);
+        expect(addPawn(gameboard, 6))
+        .toEqual([
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","","Y"]
+        ]);
+    })
+    it("should add a pawn to row 0 column 0 when gameboard is empty", function(){
+        var gameboard = [
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""]
+        ];
+
+        expect(addPawn(gameboard, 0))
+        .toEqual([
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["Y","","","","","",""]
+        ]);
+    })
+    it("should add a pawn to row 4 column 0 when column 0 is full", function(){
+        var gameboard = [
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["Y","","","","","",""]
+        ];
+
+        expect(addPawn(gameboard, 0))
+        .toEqual([
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""]
+        ]);
+    })
+
+    it("should add a pawn to row 3 column 0 when column 0 is full", function(){
+        var gameboard = [
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""]
+        ];
+
+        expect(addPawn(gameboard, 0))
+        .toEqual([
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""]
+        ]);
+    })
+
+    it("should fill the column", function(){
+        var gameboard = [
+            ["","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""]
+        ];
+
+        expect(addPawn(gameboard, 0))
+        .toEqual([
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""]
+        ]);
+    })
+
+    it("should throw the column is full", function(){
+        var gameboard = [
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""],
+            ["Y","","","","","",""]
+        ];
+        expect(function() {addPawn(gameboard, 0)}).toThrow("The column is full");
+    })
+    
+    it("should return a error message when the gameboard is full", function(){
+        var gameboard = [
+            ["Y", "Y", "Y", "Y", "Y", "Y", "Y"],
+            ["Y", "Y", "Y", "Y", "Y", "Y", "Y"],
+            ["Y", "Y", "Y", "Y", "Y", "Y", "Y"],
+            ["Y", "Y", "Y", "Y", "Y", "Y", "Y"],
+            ["Y", "Y", "Y", "Y", "Y", "Y", "Y"],
+            ["Y", "Y", "Y", "Y", "Y", "Y", "Y"]
+        ];
+        expect(function() {addPawn(gameboard, 2)}).toThrow("The gameboard is full");
     })
 })
