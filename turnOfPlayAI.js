a4859af1fe369a6836493aba4f45477a114b5a8e
NadiaPoi <nadia.poi.fou@gmail.com>
2019-04-05 13:16:54 +0200

    update turnOfPlayAI.js and add new test

--- turnOfPlayAI.js
+++ turnOfPlayAI.js
@@ -1,7 +1,9 @@
 const addPawn = require("./addPawn");
 
 function turnOfPlayAI(gameboard, randomPosition) {
-  //randomPosition = Math.floor(Math.random() * gameboard[0].length);
+  if (randomPosition === 0 && gameboard[0][randomPosition] !== "") {
+    randomPosition = Math.floor(Math.random() * gameboard[0].length) + 1;
+  }
   addPawn(gameboard, randomPosition, "R");
   console.log(gameboard);
   return gameboard;
--- turnOfPlayAI.test.js
+++ turnOfPlayAI.test.js
@@ -22,4 +22,26 @@ describe("turnOfPlayAI", function() {
       ["R", "", "", "Y", "", "", ""]
     ]);
   });
+
+  it("should add a red pawn to another colum when colum is full", function() {
+    var gameboard = [
+      ["Y", "", "", "", "", "", ""],
+      ["R", "", "", "", "", "", ""],
+      ["Y", "", "", "", "", "", ""],
+      ["R", "", "", "", "", "", ""],
+      ["R", "", "R", "", "", "", ""],
+      ["Y", "", "Y", "Y", "", "", ""]
+    ];
+
+    var randomPosition = Math.floor(Math.random() * 0) + 1;
+
+    expect(turnOfPlayAI(gameboard, randomPosition)).toEqual([
+      ["Y", "", "", "", "", "", ""],
+      ["R", "", "", "", "", "", ""],
+      ["Y", "", "", "", "", "", ""],
+      ["R", "", "", "", "", "", ""],
+      ["R", "", "R", "", "", "", ""],
+      ["Y", "R", "Y", "Y", "", "", ""]
+    ]);
+  });
 });
