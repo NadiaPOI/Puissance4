4f862e40e334f1ef6f3a204cab7824ee2d3b2fe9
NadiaPoi <nadia.poi.fou@gmail.com>
2019-04-05 14:59:04 +0200

    update turnOfPlayAI.js add new function findEmptyColumn()

--- turnOfPlayAI.js
+++ turnOfPlayAI.js
@@ -1,12 +1,24 @@
 const addPawn = require("./addPawn");
 
 function turnOfPlayAI(gameboard, randomPosition) {
-  if (randomPosition === 0 && gameboard[0][randomPosition] !== "") {
-    randomPosition = Math.floor(Math.random() * gameboard[0].length) + 1;
+  if (gameboard[0][randomPosition] !== "") {
+    var emptiesColumns = findEmptyColumn(gameboard[0]);
+    randomPosition =
+      emptiesColumns[Math.floor(Math.random() * emptiesColumns.length)];
   }
   addPawn(gameboard, randomPosition, "R");
   console.log(gameboard);
   return gameboard;
+}
+
+function findEmptyColumn(row) {
+  var emptiesColumns = [];
+  for (var column = 0; column < row.length; column++) {
+    if (row[column] === "") {
+      emptiesColumns.push(column);
+    }
+  }
+  return emptiesColumns;
 }
 
 module.exports = turnOfPlayAI;
