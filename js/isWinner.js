24307d374c62aa3b125258af870678458df7f57e
NadiaPoi <nadia.poi.fou@gmail.com>
2019-05-01 17:35:04 +0200

    add new test at isWinner.test.js and update isWinner

--- js/isWinner.js
+++ js/isWinner.js
@@ -1,14 +1,16 @@
 export function isWinner(gameboard, row, col, colorPlayer) {
   let alignedPawns = 0;
+  let lastRow = gameboard.length - 1;
+  const currentCol = col;
 
-  //horizontally
-  for (let indexRow = gameboard.length - 1; indexRow >= 0; indexRow--) {
+  //vertically
+  for (let indexRow = lastRow; indexRow >= 0; indexRow--) {
     alignedPawns =
       gameboard[indexRow][col] === colorPlayer ? alignedPawns + 1 : 0;
     if (alignedPawns >= 4) return true;
   }
 
-  //vertically
+  //horizontally
   for (
     let indexColumn = 0;
     indexColumn < gameboard[row].length;
@@ -19,28 +21,38 @@ export function isWinner(gameboard, row, col, colorPlayer) {
     if (alignedPawns >= 4) return true;
   }
 
-  //diagonally left down
-  for (let indexRow = row; indexRow < gameboard.length; indexRow++) {
-    alignedPawns =
-      gameboard[indexRow][col] === colorPlayer ? alignedPawns + 1 : 0;
-    col--;
-    if (alignedPawns >= 4) return true;
-  }
-
   //diagonally right down
   for (let indexRow = row; indexRow < gameboard.length; indexRow++) {
     alignedPawns =
       gameboard[indexRow][col] === colorPlayer ? alignedPawns + 1 : 0;
-    col++;
+    col = col + 1;
     if (alignedPawns >= 4) return true;
+    if (indexRow === lastRow && col >= gameboard[indexRow].length) {
+      col = currentCol;
+    }
+  }
+
+  //diagonally left down
+  for (let indexRow = row; indexRow < gameboard.length; indexRow++) {
+    alignedPawns =
+      gameboard[indexRow][col] === colorPlayer ? alignedPawns + 1 : 0;
+    col = col - 1;
+    if (alignedPawns >= 4) return true;
+    if (indexRow === lastRow && col >= gameboard[indexRow].length) {
+      col = currentCol;
+    }
   }
 
   //diagonally left up
   for (let indexRow = row; indexRow >= 0; indexRow--) {
     alignedPawns =
       gameboard[indexRow][col] === colorPlayer ? alignedPawns + 1 : 0;
-    col--;
+    col = col - 1;
     if (alignedPawns >= 4) return true;
+    if (indexRow === 0 && col === 0) {
+      col = currentCol;
+    }
   }
+
   return false;
 }
