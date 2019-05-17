730c05d12abc4b33c7096b16b31641c8d5ab22e3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-17 19:10:53 +0000

    Deploy NadiaPOI/Puissance4 to github.com/NadiaPOI/Puissance4.git:gh-pages

--- js/addPawns.test.js
+++ js/addPawns.test.js
@@ -24,4 +24,29 @@ describe("addPawns", () => {
       'src="./img/red.png"'
     );
   });
+
+  it("should add yellow pawn at the previous row when row 5 is empty", () => {
+    document.body.innerHTML = '<table id="gameboard"></table>';
+    let gameboard = [
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""],
+      ["", "", "", "", "", "", ""]
+    ];
+
+    initGame(gameboard);
+
+    let column = document.querySelector("tr:nth-child(6) td:nth-child(1)")
+      .firstChild;
+    let previousColumn = document.querySelector(
+      "tr:nth-child(5) td:nth-child(1)"
+    ).firstChild;
+    addPawns(column, gameboard);
+    addPawns(previousColumn, gameboard);
+
+    expect(column.src).toBe("http://localhost/img/yellow.png");
+    expect(previousColumn.src).toBe("http://localhost/img/yellow.png");
+  });
 });
