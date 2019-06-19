786b08cce40144007b10960bf3993b41bf887a1b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-19 13:08:26 +0000

    Deploy NadiaPOI/Puissance4 to github.com/NadiaPOI/Puissance4.git:gh-pages

--- js/gameboard/restartGame.test.js
+++ js/gameboard/restartGame.test.js
@@ -1,19 +1,24 @@
-import { restartGame } from './restartGame.controller';
-
-describe('restartGame', () => {
-  it('should generate a new gameboard when you click to the button start again', () => {
-    document.body.innerHTML = `<table id="gameboard></table>
-    <section id="displayWinner" s>
+beforeEach(() => {
+  document.body.innerHTML = `<table id="gameboard></table>
+    <section id="displayWinner">
     <p>Great !! Yellow player win !</p>
     <button id="restart">Start again</button>
-    </section>`;
+    </section>
+    <aside>
+      <form>
+        <input type="text" name="row" id="row" value="6">
+        <input type="text" name="col" id="col" value="7">
+        <input type="submit" value="Generate gameboard">
+      </form>
+    </aside>
+    `;
+});
 
-    restartGame();
-
+describe('restartGame', () => {
+  it('should display a form to submit a board when you click to the button start again', () => {
     let btn = document.getElementById('restart');
-    window.location.reload = jest.fn();
     btn.click();
 
-    expect(window.location.reload).toHaveBeenCalled();
+    expect(document.querySelector('aside')).toMatchSnapshot();
   });
 });
