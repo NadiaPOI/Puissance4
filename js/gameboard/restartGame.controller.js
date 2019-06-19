7ed0399666c3b55c4304cdc84b62656e97e65940
NadiaPoi <nadia.poi.fou@gmail.com>
2019-06-19 11:19:55 +0200

    update restartGame and addPawns

--- js/gameboard/restartGame.controller.js
+++ js/gameboard/restartGame.controller.js
@@ -1,11 +1,10 @@
-// import { submitLengthGameboard } from './submitLengthGameboard.controller.js';
+import { submitLengthGameboard } from './submitLengthGameboard.controller.js';
 
 export function restartGame() {
   let btn = document.getElementById('restart');
   btn.addEventListener('click', () => {
-    window.location.reload(true);
-    /* document.getElementById('displayWinner').style.display = 'none';
-    document.getElementById('gameboard').style.display = 'none';
-    submitLengthGameboard(); */
+    document.getElementById('gameboard').remove();
+    document.getElementById('displayWinner').style.display = 'none';
+    submitLengthGameboard();
   });
 }
--- js/getNewBoard.js
+++
@@ -1,9 +0,0 @@
-export function getNewBoard() {
-  let btn = document.createElement('button');
-  btn.innerHTML = 'Start again';
-  btn.setAttribute('id', 'restart');
-  document.getElementById('displayWinner').appendChild(btn);
-  btn.addEventListener('click', () => {
-    window.location.reload(true);
-  });
-}
