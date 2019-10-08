14c5bf1a1922e01441b70426431e32fd52f055c7
NadiaPoi <nadia.poi.fou@gmail.com>
2019-10-08 13:54:54 +0200

    refactoring

--- js/AI/turnOfPlayAI.logic.js
+++ js/AI/turnOfPlayAI.logic.js
@@ -4,30 +4,17 @@ export function turnOfPlayAI(gameboard, randomColumn) {
   return addPawn(gameboard, randomColumn, 'R');
 }
 
-// eslint-disable-next-line consistent-return
 export function findEmptyColumn(gameboard, randomFunction) {
-  /* let emptiesColumns = [];
-  for (let indexRow = gameboard.length - 1; indexRow >= 0; indexRow--) {
-    if (gameboard[indexRow].includes('')) {
-      let row = gameboard[indexRow];
-      for (let column = 0; column < row.length; column++) {
-        if (row[column] === '') {
-          emptiesColumns.push(column);
-        }
-      }
-      let randomColumn = emptiesColumns[Math.floor(randomFunction() * (emptiesColumns.length - 1))];
-      return randomColumn;
-    }
-  } */
-
   let emptiesRows = gameboard.filter((row) => row.includes(''));
   let indexLastRow = emptiesRows.length - 1;
 
-  let emptiesColumns = gameboard[indexLastRow].map((column, index) => {
-    if (column === '') return index;
-    return null;
-  }).filter(column => column !== null);
+  let emptiesColumns = gameboard[indexLastRow]
+    .map((column, index) => {
+      if (column === '') return index;
+      return null;
+    })
+    .filter((column) => column !== null);
 
-  let randomColumn = emptiesColumns[Math.floor(randomFunction() * (emptiesColumns.length - 1))];
+  let randomColumn =    emptiesColumns[Math.floor(randomFunction() * (emptiesColumns.length - 1))];
   return randomColumn;
 }
--- js/gameboard/generateGameboard.logic.js
+++ js/gameboard/generateGameboard.logic.js
@@ -1,20 +1,4 @@
 export function generateGameboard(lengthRow, lengthColumn) {
-  /* if (lengthRow < 0 || lengthColumn < 0) {
-    throw new Error('Invalid parameters');
-  }
-
-  const tabRow = new Array(lengthRow);
-
-  for (let line = 0; line < tabRow.length; line++) {
-    let newRow = new Array(lengthColumn);
-    tabRow[line] = newRow;
-    for (let column = 0; column < newRow.length; column++) {
-      newRow[column] = '';
-    }
-  }
-
-  return tabRow; */
-
   let gameboard = new Array(lengthRow).fill(null).map(
     () => (new Array(lengthColumn).fill(''))
   );
